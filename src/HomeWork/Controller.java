b448bdaf3b2dbb543463d66717f701c54066e176
Denis Azhymov <azhimov93@gmail.com>
2016-11-23 16:06:17 +0200

    Init

--- src/HomeWork/Controller.java
+++ src/HomeWork/Controller.java
@@ -2,4 +2,20 @@ package HomeWork;
 
 
 public class Controller {
+    public API []apis = {new BookingComAPI(), new TripAdvisorAPI(), new GoogleAPI()};
+
+    public   Room[] requstRooms (int price,int persons, String city, String hotel){
+        Room[] freeRooms= new Room[1];
+
+    return freeRooms;
+    }
+    public Room[] check (API api1, API api2){
+        Room[] checked=new Room[10];
+        return checked;
+    }
+
+
+
+
 }
+
--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -1,3 +1,7 @@
 package HomeWork;
 public class Main {
+    public static void main(String[] args) {
+        Controller cont = new Controller();
+        cont.requstRooms(500,350,"New York","Hilton");
+    }
 }
