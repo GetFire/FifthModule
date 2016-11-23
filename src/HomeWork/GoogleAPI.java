f6a4503e93c09626884f9aaaeb607496b083ecfb
Denis Azhymov <azhimov93@gmail.com>
2016-11-23 14:22:14 +0200

    Init

--- src/HomeWork/GoogleAPI.java
+++ src/HomeWork/GoogleAPI.java
@@ -1,4 +1,9 @@
 package HomeWork;
 
 public class GoogleAPI implements API {
+    Room[]rooms;
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[5];
+    }
 }
--- src/HomeWork/TripAdvisorAPI.java
+++ src/HomeWork/TripAdvisorAPI.java
@@ -1,6 +1,7 @@
 package HomeWork;
 
 public class TripAdvisorAPI implements API {
+    Room[]rooms;
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         return new Room[5];
