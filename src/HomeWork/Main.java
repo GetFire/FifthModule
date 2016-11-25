6192e332ce6207c181beb621f4cdea25165adbd8
Denis Azhymov <azhimov93@gmail.com>
2016-11-25 17:04:34 +0200

    Init

--- src/HomeWork/Main.java
+++ src/HomeWork/Main.java
@@ -3,54 +3,36 @@ package HomeWork;
 import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         Controller controller = new Controller();
 
-        System.out.println();
-        System.out.println();
         System.out.println("----------------Запрашиваем комнаты по параметрам-----------------");
-        Room[]param=controller.requstRooms(150,1,"Лас-Вегас","The Cosmopolitan");
+        Room[] param = controller.requstRooms(150, 1, "Лас-Вегас", "The Cosmopolitan");
         System.out.println(Arrays.toString(param));
-        for (int i = 0; i < param.length; i++) {
-            if(param[i]!=null)
-                System.out.println(i+1+"."+param[i].toString());
-        }
-//
-//
-        System.out.println("------------BookingAPI--------------------");
-        Room[] s = new BookingComAPI().getAll();
-        for (int i = 0; i < s.length; i++) {
-            System.out.println(s[i]);
-        }
         System.out.println();
 
+        Room[] param1 = controller.requstRooms(150, 2, "New York", "Hilton");
+        System.out.println(Arrays.toString(param1));
+        System.out.println();
 
-        System.out.println("------------GoogleAPI--------------------");
-        Room[] s1 = new GoogleAPI().getAll();
-        for (int i = 0; i < s1.length; i++) {
-            System.out.println(s1[i]);
-        }
-            System.out.println();
-
-
+        Room[] param3 = controller.requstRooms(200, 1, "Дубай", "Atlantis The Palm");
+        System.out.println(Arrays.toString(param3));
+        System.out.println();
 
 
         System.out.println("------------СравнениеAPI--------------------");
         Room[] sad = controller.check(new BookingComAPI(), new GoogleAPI());
-        for (int i = 0; i < sad.length; i++) {
-            System.out.println(i+1+"."+sad[i]);
-        }
-        System.out.println(Arrays.asList(Arrays.toString(sad)));
+        Room[] sad1 = controller.check(new BookingComAPI(), new TripAdvisorAPI());
+        Room[] sad2 = controller.check(new TripAdvisorAPI(), new GoogleAPI());
 
+        System.out.println(Arrays.toString(sad));
+        System.out.println();
 
+        System.out.println(Arrays.toString(sad1));
+        System.out.println();
 
-
-
-
-
-
-
-
+        System.out.println(Arrays.toString(sad2));
+        System.out.println();
 
 
     }
